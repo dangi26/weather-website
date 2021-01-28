cd1987eab161b34de69b50be7123bbdf2c9974da
gopal <namegd94@gmail.com>
2021-01-28 23:54:10 +0530

    update forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude,longitude, callback)=>{
         }else if(body.error){
            callback('unable to find location. try again', undefined);
         }else{
-            callback(undefined,body.daily.data[0].summary+ ' it is currently '+body.currently.temperature+' degree out. there is a '+body.currently.precipProbability+'% chance of rain.')
+            callback(undefined,body.daily.data[0].summary+ ' it is currently '+body.currently.temperature+' degree out. the high today is ' +body.daily.data[0].temperatureHigh + 'with a low of '+ body.daily.data[0].temperatureLow + '. there is a '+body.currently.precipProbability+'% chance of rain.')
         }
         
     })
