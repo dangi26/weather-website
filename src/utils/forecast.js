cc782466ecfaa8098c5b621f2703a0d2070d026e
gopal <namegd94@gmail.com>
2021-01-28 17:35:09 +0530

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+const forecast = (latitude,longitude, callback)=>{
+
+    const url = 'https://api.darksky.net/forecast/2a829905beb33559332a9dc01cf0c101/' +latitude+ ',' +longitude
+
+    request({url, json: true}, (error, {body})=>{
+
+        if(error){
+            callback('unable to connect to weather services', undefined);
+        }else if(body.error){
+           callback('unable to find location. try again', undefined);
+        }else{
+            callback(undefined,body.daily.data[0].summary+ ' it is currently '+body.currently.temperature+' degree out. there is a '+body.currently.precipProbability+'% chance of rain.')
+        }
+        
+    })
+    
+}
+module.exports = forecast
